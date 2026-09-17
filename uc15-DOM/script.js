dbca93db09a43a2f9479d16f3c7c094b7836b259
gabrielwq7 <gabrielaraujodossantos121110@gmail.com>
2026-09-17 10:22:40 -0300

    Add files via upload

---
+++ uc15-DOM/script.js
@@ -0,0 +1,11 @@
+function mudartexto() {
+    document.getElementById
+    ("titulo").textContent = "voce clicou no botao";
+
+}
+
+function mudarcor() {
+    document.getElementById("mensagem").style.color =
+    "blue";
+}
+
